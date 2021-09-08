12c19182ca2cb2dc709f7f1a3a04bba632f526f0
Corinne <corinne.boesch@gmail.com>
2021-09-08 15:53:37 +0100

    updates on interface

--- src/Interface.js
+++ src/Interface.js
@@ -1,1 +1,20 @@
-document.addEventListener('DOMContentLoaded', () => {});
+document.addEventListener('DOMContentLoaded', () => {
+  const noteBook = new NoteBook();
+  noteBook.saveNote();
+
+  // one route that connects the textarea and passes it as argument of save note
+
+  document.querySelector('#submit').addEventListener('click', () => {
+    noteBook.saveNote();
+  });
+
+  // returns all elements abbreviated - should be linked to href
+  document.querySelector('#display-note-list').innerText =
+    noteBook.abbreviatedText();
+
+  // parameter should be linked  to empty href of #display-note-list
+  document.querySelector('#display-note-list').addEventListener('click', () => {
+    document.querySelector('#display-full-note').innerText =
+      noteBook.fullText(0);
+  });
+});
