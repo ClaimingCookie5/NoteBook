f554e94dd3c53ff3d426e6a650396065576bc4cb
ian k <kickthechair@gmail.com>
2021-09-08 17:44:56 +0100

    current working for note abbreviations and linking through to full note descriptions

--- src/Interface.js
+++ src/Interface.js
@@ -1,11 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
   const noteBook = new NoteBook();
-  noteBook.saveNote();
-
+  noteBook.saveNote('test1');
+  noteBook.saveNote('test2');
+  noteBook.saveNote('test3');
   // one route that connects the textarea and passes it as argument of save note
 
   document.querySelector('#submit').addEventListener('click', () => {
-    noteBook.saveNote();
+    noteBook.saveNote('test4');
   });
 
   // returns all elements abbreviated - should be linked to href
@@ -13,8 +14,22 @@ document.addEventListener('DOMContentLoaded', () => {
     noteBook.abbreviatedText();
 
   // parameter should be linked  to empty href of #display-note-list
-  document.querySelector('#display-note-list').addEventListener('click', () => {
-    document.querySelector('#display-full-note').innerText =
-      noteBook.fullText(0);
-  });
+  // document.querySelector('#display-note-list').addEventListener('click', () => {
+  //   document.querySelector('#display-full-note').innerText =
+  //     noteBook.fullText();
+  // });
+
+
+
+
+  // const abbrList = document.querySelectorAll('#abbreviated-list');
+  
+  //   abbrList.forEach(function(abbrList){
+  //     abbrList.addEventListener('click', checkIndex);
+  //   })
+
+  //   function checkIndex(event){
+  //   console.log( Array.from(abbrList).indexOf(event.target) );
+  //   }
+  // };
 });
