0535b6b8eba5e77b3daf07dbacbd4e2e2ed07149
ian k <kickthechair@gmail.com>
2021-09-09 15:32:33 +0100

    working on id's

--- src/Interface.js
+++ src/Interface.js
@@ -4,30 +4,59 @@ document.addEventListener('DOMContentLoaded', () => {
     noteBook.abbreviatedText();
   }
   const noteBook = new NoteBook();
-
-  console.log(noteBook);
+  let idSelect = 0;
 
   addListTag = () => {
     let list = document.querySelector('#list-container');
     let li = document.createElement('li');
-    let link = document.createElement('a');
+    //let link = document.createElement('a');
     let noteList = noteBook.abbreviatedText();
     noteList.forEach(note => {
       for (let i = 0; i <= noteList.length; i++) {
-        link.href = note;
+        let link = document.createElement('a');
+        link.href = `#${note}`;
+        link.setAttribute('id', `${i}`) //i starts at 1. NOT 0
         link.innerText = note;
         li.appendChild(link);
         list.appendChild(li);
+        link.addEventListener('click', () => {
+          idSelect = i
+          console.log(idSelect);
+        });
       }
     });
   }
+
+
+  // const result = document.createElement('h1')
+  // document.querySelector('body').appendChild(result)
+
+  // addListTag = () => {
+  //   let noteList = noteBook.abbreviatedText();
+
+  //   for (let i = 0; i <= noteBook.noteList.length; i++) {
+  //     let link = document.createElement('a');
+  //     link.href = noteBook.abbreviatedText[i]
+  //     link.setAttribute('id', `${i}`)
+  //     link.addEventListener('click', () => {
+  //       idSelect = i
+  //       console.log(idSelect);
+  //       });
+  //     result.appendChild(link)
+  //   }
+  // }
   
+
+
+
+
   updateList();
   // one route that connects the textarea and passes it as argument of save note
   document.querySelector('#submit').addEventListener('click', () => {
     let note = document.querySelector('#create-note').value;
     noteBook.saveNote(note)
     addListTag()
+    document.querySelector('#create-note').value = "";
   });
 
   // returns all elements abbreviated - should be linked to href
@@ -35,8 +64,8 @@ document.addEventListener('DOMContentLoaded', () => {
   //   noteBook.abbreviatedText();
 
   // parameter should be linked  to empty href of #display-note-list
-  document.querySelector('#display-note-list').addEventListener('click', () => {
-    document.querySelector('#display-full-note').innerText =
-      noteBook.fullText(0);
-  });
+  // document.querySelector('#display-note-list').addEventListener('click', () => {
+  //   document.querySelector('#display-full-note').innerText =
+  //     noteBook.fullText(0);
+  // });
 });
